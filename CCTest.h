39f1c9baa60c6181cff034ebbfe1ecbe435acee0
amina alavi <aminaalavi@aminas-MacBook-Air.local>
2013-12-06 12:13:48 -0500

    added a test class

---
+++ CCTest.h
@@ -0,0 +1,13 @@
+//
+//  CCTest.h
+//  Git Test
+//
+//  Created by amina alavi on 12/6/13.
+//  Copyright (c) 2013 code coalition. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CCTest : NSObject
+
+@end
---
+++ CCTest.m
@@ -0,0 +1,13 @@
+//
+//  CCTest.m
+//  Git Test
+//
+//  Created by amina alavi on 12/6/13.
+//  Copyright (c) 2013 code coalition. All rights reserved.
+//
+
+#import "CCTest.h"
+
+@implementation CCTest
+
+@end
